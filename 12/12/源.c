8b24ef1924caaa10e27949b28c7abb791c2eae4c
yuban13857hao <zxcvbnm148585762@outlook.com>
2025-04-02 22:52:39 +0800

    typedef

---
+++ 12/12/源.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a[5] = {0};
+//	a[0] = 1;
+//	a[1] = 2;
+//	a[2] = 3;
+//	a[3] = 4;
+//	a[4] = 5;
+//	printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]);
+//
+//	return 0;
+//}
+typedef unsigned int uint_32;
+int main()
+{
+	unsigned int a = 0;
+	uint_32 b = 0;
+	return 0;
+}
