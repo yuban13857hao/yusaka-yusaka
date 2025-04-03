7f73f852c65f5f75f886f35a1be39d082d8a36e5
yuban13857hao <zxcvbnm148585762@outlook.com>
2025-04-03 15:19:19 +0800

    指针变量

--- 15/15/源.c
+++ 15/15/源.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 int main()
 {
 	int a = 10;//向内存申请4个字节。储存10
@@ -5,5 +6,7 @@ int main()
 	//printf("%p\n",&a);
 	int* p = &a;
 	//p就是指针变量（*说明p是指针变量，int说明p指向的对象是int类型的）
+	*p = 20;//解引用操作符，意思就是通过p中存放的地址，找到p所指向的对象，*p就是p指向的对象，或者可以粗略的理解为*p==a
+	printf("%d\n", *p);
 	return 0;
 }
