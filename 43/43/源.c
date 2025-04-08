244d96191e3097540e8ba3434b2bbaf417a05292
yuban13857hao <zxcvbnm148585762@outlook.com>
2025-04-08 13:26:41 +0800

    goto

---
+++ 43/43/源.c
@@ -0,0 +1,29 @@
+//飞雷神语句，别用，而且不能跨函数
+#include<stdio.h>
+//int main()
+//{
+//	a:
+//		printf("aaa");
+//	    printf("bbb");
+//	goto a;
+//	return 0;
+//}
+//经典死循环
+
+//适合场景：
+for()
+for()
+{
+	for ()
+	{
+		for ()
+		{
+			if(disaster)
+				goto a
+		}
+	}
+}
+……
+a:
+if(disaster)
+……
