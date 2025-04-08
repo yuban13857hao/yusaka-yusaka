ddd5269c827c5953834a7ad424a1836f0a5d07db
yuban13857hao <zxcvbnm148585762@outlook.com>
2025-04-08 22:26:25 +0800

    输入两个整形，交换其值

---
+++ 46/46/源.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void e(int* x, int* y)//形参
+{
+	int z = *x;
+	*x = *y;
+	*y = z;
+}
+//实参
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf_s("%d%d", &a, &b);
+	e(&a, &b);
+	printf("%d%d", a, b);
+	return 0;
+}
