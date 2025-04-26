c6d93f1322b0ae1e3d48b90d0a85cf4158003549
yuban13857hao <zxcvbnm148585762@outlook.com>
2025-04-26 14:54:59 +0800

    找出5位数中所有的变种水仙花数

---
+++ 99/99/源.c
@@ -0,0 +1,21 @@
+//变种水仙花数
+//找出5位数中所有的变种水仙花数
+#include <stdio.h>
+
+int main() {
+    for (int i = 10000; i < 100000; i++) 
+    {
+        
+        int part1 = (i / 10000) * (i % 10000);  
+        int part2 = (i / 1000) * (i % 1000);     
+        int part3 = (i / 100) * (i % 100);       
+        int part4 = (i / 10) * (i % 10);        
+        int sum = part1 + part2 + part3 + part4;
+
+        if (i == sum) 
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
